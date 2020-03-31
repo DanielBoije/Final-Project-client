fc5c117472b92feb6f49fc2bfa6fc0bc31bf0b61
Aleksi1K <59920287+Aleksi1K@users.noreply.github.com>
2020-03-31 10:33:18 +0300

    kesken

--- src/komponentit/reitit/Lista.js
+++ src/komponentit/reitit/Lista.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import ListaTiedot from "./lista/ListaTiedot"
+import './Lista.css'
 
 class Lista extends Component {
     render() {
         return (
-            <div>
+            <div className="taustakuva">
                 <ListaTiedot/>
             </div>
         );
--- src/komponentit/reitit/lista/ListaTiedot.js
+++ src/komponentit/reitit/lista/ListaTiedot.js
@@ -1,55 +1,98 @@
 import React, { Component } from 'react';
-import {getData} from "./listaService"
+import { getData } from "./listaService"
 import moment from "moment"
+// import { colors } from 'material-ui/styles';
+import './ListaTiedot.css'
+import { Table } from "react-bootstrap"
+// import { Paper } from '@material-ui/core';
+
+
 class ListaPohja extends Component {
 
     state = {
-        Data:[]
+        Data: []
     }
-    
-    componentDidMount=()=>{
-        this.importData();        
-     }
-     importData=()=>{
-         getData().then( async res => {
-             console.log(res.data)
-             await this.setState({Data:res.data})
-             console.log(this.state)
-           });
-     }
+
+    componentDidMount = () => {
+        this.importData();
+    }
+    importData = () => {
+        getData().then(async res => {
+            console.log(res.data)
+            await this.setState({ Data: res.data })
+            console.log(this.state)
+        });
+    }
+
+
 
     render() {
-        var lista = this.state.Data.map( b => {
-            return ( 
-            <tr>
-                <td key={b.id} value={b.id}>{b.id}</td>
-                <td key={b.id} value={b.id}>{moment(b.pvm).format('DD/MM/YYYY')}</td>
-                <td key={b.id} value={b.id}>{b.nimi}</td>
-                <td key={b.id} value={b.id}>{b.tyovuoro}</td>
-                <td key={b.id} value={b.id}>{b.tuotenimi}</td>
-            </tr>) ;
-            })
+        var lista = this.state.Data.map(b => {
+            return (
+                <tr>
+                    <td key={b.id} value={b.id}>{b.id}</td>
+                    <td key={b.id} value={b.id}>{moment(b.pvm).format('DD/MM/YYYY')}</td>
+                    <td key={b.id} value={b.id}>{b.nimi}</td>
+                    <td key={b.id} value={b.id}>{b.tyovuoro}</td>
+                    <td key={b.id} value={b.id}>{b.tuotenimi}</td>
+                    <td key={b.id} value={b.id}>{b.tehtytunnit}</td>
+                    <td key={b.id} value={b.id}>{b.tehdytkappaleet}</td>
+                    <td key={b.id} value={b.id}>{b.hairiokesto}</td>
+                </tr>);
+        })
         return (
-            <div style={taulukko}>
+          
+            <div className="font">
+            <Table striped bordered hover variant="dark">
+                
+                    <tr>
+                        <th>#</th>
+                        <th>PÄIVÄMÄÄRÄ</th>
+                        <th>LINJA</th>
+                        <th>TYÖVUORO</th>
+                        <th>TUOTTEEN NIMI</th>
+                        <th>KESTO</th>
+                        <th>TEHDYT KAPPALEET</th>
+                        <th>HÄIRIÖN KESTO</th>
+                    </tr>
+                    <tbody>{lista}</tbody>
+                
+            </Table>
+            </div>
+          
+
+            /* /*<div className="table">
                 <br></br><br></br><br></br><br></br><br></br><br></br>
                 <table>
                     <tr>
-                        <th>ID</th>
+                        <th className="td">ID</th>
                         <th>Päivämäärä</th>
                         <th>Linja</th>
                         <th>Työvuoro</th>
                         <th>Tuotteen nimi</th>
+                        <th>Kesto</th>
+                        <th>Tehdyt kappaleet</th>
+                        <th>Häiriön kesto</th>
                     </tr>
-                        {lista}                    
+                    {lista}
                 </table>
-            </div>
+            </div> */ 
         );
-    }
+   }
 }
 
-const taulukko = {
-    margin: "100px",
-    display: 'flex', 
-    justifyContent: 'center'
-}
+// const taulukko = {
+//     margin: "100px",
+//     display: 'flex', 
+//     justifyContent: 'center',
+//     bordercollapse: 'collapse',
+//     width: '100%'
+
+// }
+
+
+
+
+
+
 export default ListaPohja;
